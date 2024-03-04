640338c5fa14f0ac79ff83d835cf7cfbf1f7b8f5
ajaibajwa <43457475+ajaibajwa@users.noreply.github.com>
2024-03-04 13:28:41 -0500

    Updated Front-end code

---
+++ ecommerce-frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,16 @@
+export default {
+    oidc: {
+        clientId: '0oa6hnwwonsOm1f6n5d7',
+        issuer: 'https://dev-3553715.okta.com/oauth2/default',
+        redirectUri: 'https://e-frontend.azurewebsites.net/home',
+        postLogoutRedirectUri: 'https://e-frontend.azurewebsites.net/login',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
+ // OLD CREDENTIALS
+// '0oa6ab9e99dOuneyG5d7
+// https://dev-08324756.okta.com/oauth2/default
+ 
+// NEW CREDENTIALS WITH WORKING REGISTER
+// '0oa6hnwwonsOm1f6n5d7'
+// https://dev-3553715.okta.com/oauth2/default
